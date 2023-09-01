2e2b8dd9289fa6262df83ca509ac6c7a90e03935
Jhon Calderon <jhoncalderon20174@gmail.com>
2023-08-31 21:46:42 -0500

    Revert "security, autowired"
    This reverts commit 38855d85

--- src/main/java/com/university/api/infra/security/TokenService.java
+++ src/main/java/com/university/api/infra/security/TokenService.java
@@ -3,6 +3,8 @@ package com.university.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.university.api.domain.usuarios.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,23 +19,42 @@ public class TokenService {
     @Value("${app.api.secret}")
     private String secret;
 
-    public String generateToken(Usuario u){
 
-        try{
-            System.out.println(secret);
+    public String generateToken(Usuario u) {
+        try {
+            System.out.println("try login");
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("auth0")
                     .withSubject(u.getUsername())
                     .withClaim("id", u.getId())
-                    .withExpiresAt(generateExpiredDate())
+//                    .withExpiresAt(generateExpiredDate())
                     .sign(algorithm);
-        }catch (JWTCreationException e){
-            throw new RuntimeException(e);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Generate token error", e);
         }
     }
 
-    private Instant generateExpiredDate(){
+
+    public String getSubject(String token) {
+        if(token == null){
+            throw new RuntimeException("getSubject error, token is null");
+        }
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        DecodedJWT decoded;
+        try {
+            decoded = JWT.require(algorithm)
+                    .withIssuer("auth0")
+                    .build()
+                    .verify(token);
+            return decoded.getSubject();
+        }catch (JWTVerificationException e){
+            throw new RuntimeException("Authorization error");
+        }
+    }
+
+
+    private Instant generateExpiredDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
     }
 }
