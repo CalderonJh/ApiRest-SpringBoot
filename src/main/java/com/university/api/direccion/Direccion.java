9c6934487079a18839954885c0039c0669d88f42
Jhon Calderon <jhoncalderon20174@gmail.com>
2023-09-12 08:43:02 -0500

    implementando inscripciones a cursos

--- src/main/java/com/university/api/direccion/Direccion.java
+++ src/main/java/com/university/api/direccion/Direccion.java
@@ -2,9 +2,7 @@ package com.university.api.Direccion;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.proxy.HibernateProxy;
 
 import java.util.Objects;
